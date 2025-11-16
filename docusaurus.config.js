949d28e1fa9d18f333abadc13fc086e4cd243233
boa-z <boa-z@outlook.com>
2025-11-16 22:18:11 +0800

    Move broken markdown hook to new config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,11 @@ const config = {
 
   // onBrokenLinks: 'throw',
   onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is in Chinese, you may want
